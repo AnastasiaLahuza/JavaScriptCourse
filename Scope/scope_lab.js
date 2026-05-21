4d270b793e651784289cf30b27021821dc5b2b2c
AnastasiaLahuza <asenka88@gmail.com>
2026-05-21 08:49:49 -0400

    Calculator

---
+++ Scope/scope_lab.js
@@ -0,0 +1,8 @@
+var globalVar = "glabal var, can outside the block";
+let globalLet = "global variable, can be seen just in a scope";
+const globalConst = "can not be reasigned, seen just in a scope, class";
+
+//see it at page
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
